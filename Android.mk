4ea42901a9e09efdf512036370e11e12a9794900
Matti Lehtimäki <matti.lehtimaki@jolla.com>
2023-10-13 20:47:12 +0000

    recording

--- Android.mk
+++ Android.mk
@@ -65,7 +65,6 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := droidmedia.cpp \
                    droidmediaconstants.cpp \
                    droidmediaconvert.cpp \
-                   droidmediarecorder.cpp \
                    allocator.cpp \
                    droidmediabuffer.cpp \
                    private.cpp
@@ -89,12 +88,14 @@ LOCAL_SHARED_LIBRARIES := libc \
 
 ifeq ($(shell test $(ANDROID_MAJOR) -ge 7 && echo true),true)
 LOCAL_SRC_FILES += droidmediacamera2.cpp \
+                   droidmediarecorder2.cpp \
                    droidmediacodec.cpp
 LOCAL_SHARED_LIBRARIES += libcamera2ndk \
                           libmediandk \
                           libnativewindow
 else
 LOCAL_SRC_FILES += droidmediacamera.cpp \
+                   droidmediarecorder.cpp \
                    droidmediacodec.cpp
 endif
 
